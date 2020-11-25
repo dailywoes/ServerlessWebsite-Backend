aaf31bbe36af070228b76997f75055cc5c73394e
dailywoes <johnrteixeira@gmail.com>
2020-11-25 00:29:34 -0500

    new

--- prod/app.py
+++ prod/app.py
@@ -1,42 +1,24 @@
+from pprint import pprint
 import json
-
-# import requests
-
-
+import boto3
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    dynamodb = boto3.resource('dynamodb',region_name='ca-central-1')
+    table = dynamodb.Table('webpage')
+    item = table.get_item(
+        Key={
+            "pageId":0,
+        }
+        )
+    table.update_item(
+        Key={
+            "pageId":0,
+        },
+        UpdateExpression='SET quantity = :val1',
+        ExpressionAttributeValues={
+            ':val1': item['Item']['quantity'] + 1
+        }
+    )
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": {"Visit_Count": str(item['Item']['quantity'] + 1)}
     }
